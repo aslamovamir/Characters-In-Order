b88a37a42149ea9698e7043adcfde2d037f95f79
Amir Aslamov <90361791+aslamovamir@users.noreply.github.com>
2022-01-30 16:00:28 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Output_TB = new System.Windows.Forms.TextBox();
+            this.Input_TB = new System.Windows.Forms.TextBox();
+            this.Submit_BTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(172, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(172, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Output:";
+            // 
+            // Output_TB
+            // 
+            this.Output_TB.Location = new System.Drawing.Point(265, 162);
+            this.Output_TB.Name = "Output_TB";
+            this.Output_TB.Size = new System.Drawing.Size(284, 20);
+            this.Output_TB.TabIndex = 2;
+            // 
+            // Input_TB
+            // 
+            this.Input_TB.Location = new System.Drawing.Point(265, 121);
+            this.Input_TB.Name = "Input_TB";
+            this.Input_TB.Size = new System.Drawing.Size(284, 20);
+            this.Input_TB.TabIndex = 3;
+            // 
+            // Submit_BTN
+            // 
+            this.Submit_BTN.Location = new System.Drawing.Point(368, 223);
+            this.Submit_BTN.Name = "Submit_BTN";
+            this.Submit_BTN.Size = new System.Drawing.Size(75, 23);
+            this.Submit_BTN.TabIndex = 4;
+            this.Submit_BTN.Text = " Submit";
+            this.Submit_BTN.UseVisualStyleBackColor = true;
+            this.Submit_BTN.Click += new System.EventHandler(this.Submit_BTN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Submit_BTN);
+            this.Controls.Add(this.Input_TB);
+            this.Controls.Add(this.Output_TB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Output_TB;
+        private System.Windows.Forms.TextBox Input_TB;
+        private System.Windows.Forms.Button Submit_BTN;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Submit_BTN_Click(object sender, EventArgs e)
+        {
+            //let's get the user input into a local variable
+            string user_input = Input_TB.Text;
+            bool in_order = true;
+
+            //let's loop through the string and see if any two neighboring chars are in order
+            for (int i = 1; i < user_input.Length; i++)
+            {
+                //first check if the char is a letter
+                //if not a letter, the output is not in order by default
+                if (!Char.IsLetter(user_input[i]) || !Char.IsLetter(user_input[i-1]))
+                {
+                    MessageBox.Show("Detected a char that is not a letter!");
+                    in_order = false;
+                    Output_TB.Text = "";
+                    break;
+                }
+                
+                if ((int)Char.ToLower(user_input[i-1]) > (int)Char.ToLower(user_input[i]))
+                {
+                    in_order = false;
+                    break;
+                }
+            }
+        
+            if (in_order)
+            {
+                Output_TB.Text = "In order";
+            } else
+            {
+                Output_TB.Text = "Not in order";
+            }
+        }
+    }
+}
